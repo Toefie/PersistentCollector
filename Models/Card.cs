64b353192dc422a4d88cf335fefea913f277aaee
lorenzo Pineda Olzheim <2304141pinedaolzheim@zuyd.nl>
2024-09-19 09:51:21 +0200

    Add project files.

---
+++ Models/Card.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace FirstMVC.Models
+
+{
+    public class Card
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = null!;
+        [Column(TypeName = "decimal(6, 2)")]
+        public decimal Price { get; set; }
+        public string? Description { get; set; }
+
+        // Foreign key naar de Collection
+        public int CollectionId { get; set; }
+
+        // Navigatie-eigenschap naar de collectie
+        public Collection Collection { get; set; } = null!;
+    }
+}
---
+++ Models/Inventory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstMVC.Models
+{
+    public class Inventory
+    {
+        public int ID { get; set; }
+        public string Name { get; set; } = null!;
+
+        // Relatie: één inventory heeft meerdere collecties
+        public ICollection<Collection> Collections { get; set; }
+    }
+}
